69ee7e08de3160865f033af2dac03e2d71eefb5e
Rajma Chaawal! <85783946+RajmaChaawal@users.noreply.github.com>
2025-08-13 13:56:40 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    static int a = 2; //Static Variable!
+    public static void main(String[] args){
+        int b = 10; //Local Variable!
+        System.out.print(Variable.a);
+        System.out.print(b);
+    }
+    
+}
